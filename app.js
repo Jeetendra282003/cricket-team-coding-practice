278d4630b5f70088321019c87f5ec1764f7a9ef9
Jeetendra282003 <125472275+Jeetendra282003@users.noreply.github.com>
2024-01-28 11:43:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
 const path = require('path')
 const app = express()
-
+app.use(express.json())
 const databasePath = path.join(__dirname, 'cricketTeam.db')
 let db = null
 
